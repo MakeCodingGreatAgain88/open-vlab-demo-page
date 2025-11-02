35c1ff86f083c27251a086320f6a956c06240091
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-11-02 16:02:47 +0800

    perf 配置自动导入依赖

--- src/components/PriceChange/index.tsx
+++ src/components/PriceChange/index.tsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 interface PriceChangeProps {
   value: number;
   showSign?: boolean;
 }
 
-const PriceChange: React.FC<PriceChangeProps> = React.memo(({ value, showSign = true }) => {
+const PriceChange = memo<PriceChangeProps>(({ value, showSign = true }: PriceChangeProps) => {
   const color = value >= 0 ? 'rgb(239, 83, 80)' : 'rgb(8, 153, 129)';
   const displayValue = showSign && value >= 0 ? `+${value.toFixed(2)}` : value.toFixed(2);
   
