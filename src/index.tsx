2c78c2642c636ecd516ecee754339a1347010409
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-11-02 16:59:52 +0800

    perf 调整样式引入顺序

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import { ConfigProvider } from 'antd';
 import zhCN from 'antd/locale/zh_CN';
-import App from './App';
 import './index.css';
+import App from './App';
 import { themeConfig } from '@config/theme';
 
 const root = ReactDOM.createRoot(
