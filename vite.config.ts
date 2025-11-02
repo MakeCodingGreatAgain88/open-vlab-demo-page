b2b3f1a062d0b1016634a97ccae31f86554668a5
MakeCodingGreatAgain <makecodinggreatagain@gmail.com>
2025-11-02 16:25:18 +0800

    feat 引入tailwindcss优化样式

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import AutoImport from 'unplugin-auto-import/vite';
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     AutoImport({
       imports: [
         'react',
